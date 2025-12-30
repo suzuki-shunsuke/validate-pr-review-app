4a6e86db51b4993b661ce9bb9c8bb82ec906c24b
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-12-30 20:28:00 +0900

    fix: remove terraform/gcloud

--- terraform/gcloud/bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "random_id" "default" {
-  byte_length = 8
-}
-
-resource "google_storage_bucket" "default" {
-  name                        = "${random_id.default.hex}-gcf-source" # Every bucket name must be globally unique
-  location                    = "US"
-  uniform_bucket_level_access = true
-}
